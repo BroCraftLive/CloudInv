8ac0a274edb98d0af11eceb870262f1da1fb8d93
James <615283@users.noreply.github.com>
2018-05-12 10:18:12 +0100

    Initial commit

---
+++ src/main/java/com/georlegacy/general/cloudinv/CloudInv.java
@@ -0,0 +1,21 @@
+//Base archetype by James Conway (615283)
+
+package com.georlegacy.general.cloudinv;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CloudInv extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
